b6c57674d38c170d8715629bba77be0f41e85396
Madhulika Itha <41178882+madhulikaitha@users.noreply.github.com>
2025-02-25 13:16:37 -0600

    Update test_string_utils.py

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -1,1 +1,18 @@
 from src.stringutils.py import upper_case, lower_case, palindrome_str, reverse_str
+
+def test_upper_case():
+  assert upper_case("really") == "REALLY"
+  assert upper_case("nooo") == "NOOO"
+
+def test_lower_case():
+  assert lower_case("WOW") == "wow"
+  assert lower_case("SERIOUSLY") == "seriously"
+
+def test_palindrome_str():
+  assert palindrome_str("level") is True
+  assert palindrome_str("mother") is False
+
+def test_reverse_str():
+  assert reverse_str("truly") == "ylurt"
+  assert reverse_str("nothing") == "gnihton"
+
