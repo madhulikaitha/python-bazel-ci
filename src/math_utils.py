2969ac6333156a6fd00244c00499a15c95e3d82c
Madhulika Itha <41178882+madhulikaitha@users.noreply.github.com>
2025-02-25 12:40:30 -0600

    Update math_utils.py

---
+++ src/math_utils.py
@@ -0,0 +1,19 @@
+"""
+Utility functions for math
+"""
+
+def add(a,b):
+  return a + b """addition"""
+
+def sub(a,b):
+  return a - b """subtraction"""
+
+def multp(a,b):
+  return a * b """multiplication"""
+
+def div(a,b):
+  if b = 0:
+    return print("Cannot divide by zero")
+  else:
+    return a / b  """division"""
+
